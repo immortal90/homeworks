31f55400a00179e3005014a9373aebf637c05c20
Oleg Shumar <immortal90@meta.com>
2016-02-22 22:03:50 +0200

    array loops: rand array with replacing min and max values

--- arrays_loops_tasks/25.php
+++ arrays_loops_tasks/25.php
@@ -1,3 +1,20 @@
 <?php
-echo rand(0,0,0,0);
-
+$arr = array();
+$i=0;
+while ($i < 10) {
+    $arr[] = rand(0,20);
+    $i++;
+}
+foreach ($arr as $number) {
+    echo $number.' | ';
+}
+$minPos = array_search(min($arr), $arr);
+$maxPos = array_search(max($arr), $arr);
+$maxVal = max($arr);
+$minVal = min($arr);
+$arr[$minPos] = $maxVal;
+$arr[$maxPos] = $minVal;
+echo ' ==> ';
+foreach ($arr as $number) {
+    echo $number.' | ';
+}
