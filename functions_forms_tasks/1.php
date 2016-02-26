0e14220bcc65f40bf5bc1ae0e8397523745aab2d
immortal90 <immortal90@meta.ua>
2016-02-26 12:52:35 +0200

    Введите здесь свой комментарий на английском

---
+++ functions_forms_tasks/1.php
@@ -0,0 +1,6 @@
+<!Doctype html>
+<form action="action.php" method="post">
+ <p>Ваше ім'я: <input type="text" name="name" /></p>
+ <p>Ваш вік: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
