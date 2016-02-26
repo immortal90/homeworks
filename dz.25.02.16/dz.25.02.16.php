084486d0cc783314c8c22f70963dec8e2ccd2d2d
immortal90 <immortal90@meta.ua>
2016-02-26 16:19:14 +0200

    DZ

---
+++ dz.25.02.16/dz.25.02.16.php
@@ -0,0 +1,17 @@
+<!Doctype html>
+1.тип password input подходит для поля password
+2.типом hidden input можно передать перменную
+3.На почту не пришол проджек урока
+<!-- 4.<!Doctype html>
+<form action="action.php" method="post">
+    <p>Ваш текст: <input type="text" name="name" /></p>
+    <p><input type="submit" /></p>
+</form> -->
+<?php
+/*$str = $_POST['name'];
+$arr = explode(' ',$str);*/
+?>
+5.прочитал
+6.если клиент знает URL объекта, который нужно создать, используем PUT
+7.при использовании передачи данных через форму из $_FILES суперглобального массива получаем файл
+8.текст - $_POST
