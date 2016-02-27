a639613043f7928b0ef82929b9a7a079d1bf4429
immortal90 <immortal90@meta.ua>
2016-02-27 14:00:32 +0200

    Drygui commit

--- dz.25.02.16/dz.25.02.16.php
+++ dz.25.02.16/dz.25.02.16.php
@@ -1,16 +1,62 @@
 <!Doctype html>
 1.тип password input подходит для поля password
 2.типом hidden input можно передать перменную
-3.На почту не пришол проджек урока
+<!--3. <?php
+/*require "function.php"
+?>
+<html>
+<head>
+
+    <style>
+        html {
+            width: 100%;
+        }
+        body{
+            width: 100%;
+        }
+        input {
+            margin: 0 0 20px 0;
+            width: 100px;
+        }
+        .wrapper {
+            margin: 0 auto;
+            width: 940px;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <h1>list images</h1>
+    <?php
+    getImages();
+    ?>
+
+    <hr/>
+
+    <form action="form.php" name="image-form" method="post" enctype="multipart/form-data">
+
+        <label for="fname">image</label>
+        <input type="text" name="text"/><br/>
+
+
+        <input type="submit" name="button" value="save" style="width: 100px; height: 30px;
+     background: red; color: #fff; border: 0; cursor: pointer" />
+
+    </form>
+
+</div>
+
+</body>
+</html>
+<?php
+$str = $_POST['text'];
+$arr = explode(' ',$str);
+?> -->
 <!-- 4.<!Doctype html>
 <form action="action.php" method="post">
     <p>Ваш текст: <input type="text" name="name" /></p>
     <p><input type="submit" /></p>
 </form> -->
-<?php
-/*$str = $_POST['name'];
-$arr = explode(' ',$str);*/
-?>
 5.прочитал
 6.если клиент знает URL объекта, который нужно создать, используем PUT
 7.при использовании передачи данных через форму из $_FILES суперглобального массива получаем файл
