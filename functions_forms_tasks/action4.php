078db11bd83ae41c66d7c586b673f7573e0be603
immortal90 <immortal90@meta.ua>
2016-03-01 15:18:43 +0200

    first commit

--- functions_forms_tasks/action4.php
+++ functions_forms_tasks/action4.php
@@ -1,12 +1,11 @@
 <?php
-$dir = '/xampp/htdocs';
 function myFunction($dir)
 {
     $files = scandir($dir);
     foreach ($files as $value) {
         echo $value . "</br>";
     }
+
 }
 
-myFunction($dir);
-
+myFunction($dir = '/xampp/htdocs');
