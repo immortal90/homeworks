e1d34601708ece57224b211de85204c0aed0b10e
immortal90 <immortal90@meta.ua>
2016-03-02 16:31:49 +0200

    first commit

---
+++ functions_forms_tasks/6.php
@@ -0,0 +1,5 @@
+<!Doctype html>
+<form action="action6.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="myfileх[]" multiple="multiple"/>
+    <p><input type="submit" value="Запилити"/></p>
+</form>
