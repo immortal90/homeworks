eeaf9b9dd67fcb9cdfd20ffa38f39297ec0f446c
immortal90 <immortal90@meta.ua>
2016-03-03 11:08:05 +0200

    Drygui commit

--- dz.27.02.16/3/2.php
+++ dz.27.02.16/3/2.php
@@ -1,8 +1,10 @@
-<html lang="ru">
+
+<?php
+require ('1.php');?><html lang="ru">
 <body>
 <p>Вибeріть валюту</p>
 <form action="1.php" method="post">
-    <input type="submit" name="grn" value="В гривнях" >
+    <input type="submit" name="uah" value="В гривнях" >
     <input type="submit" name="usd" value="В доларах" >
 </form>
 </body>
