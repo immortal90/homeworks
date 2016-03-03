03e0b3ea483fc13db11d20945790ceca06a12af3
immortal90 <immortal90@meta.ua>
2016-03-03 10:50:59 +0200

    Drygui commit

--- dz.27.02.16/3/1.php
+++ dz.27.02.16/3/1.php
@@ -1,9 +1,3 @@
-<?php header('location 2.php; Content-Type: text/html; charset=utf-8'); ?>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <title>Result</title>
-</head>
 <?php
 $path = dirname(__FILE__);
 $fh = fopen($path . DIRECTORY_SEPARATOR . 'php.csv', 'r');
@@ -11,11 +5,12 @@ while ($row = fgetcsv($fh, 0, ';'))
     if (isset($_POST['usd'])) {
         $str = $row[0] . " " . $row[1] . " " . $row[2] . " " . "<br />";
         echo $str;
+
     } else {
 
         $str = $row[0] . " " . $row[1] . " " . $row[3] . " " . "<br />";
         echo $str;
+
     }
 setcookie('Test',$str, time() + 3600, "/");
-?>
-</html>
+
