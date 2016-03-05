b6c2b9d25db25e5aa842ba2793837242930df2fe
immortal90 <immortal90@meta.ua>
2016-03-05 10:50:34 +0200

    first commit

--- dz.02.03.16/class.inc.php
+++ dz.02.03.16/class.inc.php
@@ -5,22 +5,20 @@
 abstract class Menu
 {
     public $name;
-
+    public function SetName() {
+        echo $this->name;
+    }
 
 }
 
 
 class TopMenu extends Menu
 {
-    public function getName()
-    {
-        echo $this->name;
-    }
 }
 
 class LeftMenu extends Menu
 {
-    public function setName()
+    public function SetName()
     {
         echo '</br>' . $this->name . '</br>';
     }
@@ -28,25 +26,25 @@ class LeftMenu extends Menu
 
 $new1 = new TopMenu();
 $new1->name = '<center><a href="new1.inc.php">Новина перша </a>';
-$new1->getName();
+$new1->SetName();
 $new2 = new TopMenu();
 $new2->name = '<a href="new2.inc.php"> Новина друга </a>';
-$new2->getName();
+$new2->SetName();
 $new3 = new TopMenu();
 $new3->name = '<a href="new3.inc.php"> Новина третя</a>';
-$new3->getName();
+$new3->SetName();
 $new4 = new TopMenu();
 $new4->name = '<a href="new4.inc.php"> Новина четверта</a></center>';
-$new4->getName();
+$new4->SetName();
 $new11 = new LeftMenu();
 $new11->name = '<a href="new1.inc.php">Новина перша</a>';
-$new11->setName();
+$new11->SetName();
 $new22 = new LeftMenu();
 $new22->name = '<a href="new2.inc.php">Новина друга</a>';
-$new22->setName();
+$new22->SetName();
 $new33 = new LeftMenu();
 $new33->name = '<a href="new3.inc.php">Новина третя</a>';
-$new33->setName();
+$new33->SetName();
 $new44 = new LeftMenu();
 $new44->name = '<a href="new4.inc.php">Новина четверта</a>';
-$new44->setName();
+$new44->SetName();
