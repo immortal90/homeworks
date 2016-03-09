ede4e1230bc15f8e6ec4c3d6d59b502ce4bb8731
immortal90 <immortal90@meta.ua>
2016-03-09 10:31:00 +0200

    first commit

---
+++ phpacademy_form/index.php
@@ -0,0 +1,11 @@
+<?php
+//bootstrap file
+//include function
+require 'functions.php';
+$curr = getCurrency();
+//render html
+echo render('layout', [
+    'curCurrency' => curCurrency(),
+    'currencyWidget' => currencyWidget($curr),
+    'priceWidget' => priceWidget($curr)
+]);
